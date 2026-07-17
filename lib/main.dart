0f3824773b25c5e8c7871d8aa50d62e0c71adb24
Taisho0 <merx230@gmail.com>
2026-07-17 18:15:33 +0800

    everythings working now

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_print
 
-import 'package:excelerate_app/pages/home.dart';
+import 'package:excelerate/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
